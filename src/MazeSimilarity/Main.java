f576b87980716d5a3673a54c77b47cbd0f16f4dd
Raymond An <raymondan100@gmail.com>
2020-11-06 12:06:06 -0800

    Add Depth First Search functionality (preliminary stage) for maze 1

--- src/MazeSimilarity/Main.java
+++ src/MazeSimilarity/Main.java
@@ -8,6 +8,37 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Main {
+
+    static int time;
+    static String white = "white";
+    static String gray = "gray";
+    static String black = "black";
+
+    static void DFS(Map<Integer, Vertex> vertexes) {
+        time = 0;
+        for (Map.Entry<Integer, Vertex> vertex : vertexes.entrySet()) {
+            Vertex currentVertex = vertex.getValue();
+            if (currentVertex.color.equals(white)) {
+                DFSVisit(vertexes, currentVertex);
+            }
+        }
+    }
+
+    static void DFSVisit(Map<Integer, Vertex> vertexes, Vertex u) {
+        time += 1;
+        u.discoveredTime = time;
+        u.color = gray;
+        for (int edge : u.edges) {
+            if (vertexes.get(edge).color.equals(white)) {
+                vertexes.get(edge).previous = u;
+                DFSVisit(vertexes, vertexes.get(edge));
+            }
+        }
+        u.color = black;
+        time = time + 1;
+        u.finishedTime = time;
+    }
+
     public static void main(String[] args) {
 
         try {
@@ -23,25 +54,24 @@ public class Main {
             System.out.println("Number of mazes: " + numMazes);
             System.out.println("n: " + n);
             int i = 0;
-            int j = 0;
+            int north = 0;
+            int south = 1;
+            int west = 2;
             String maze = "";
 
             ArrayList<String> mazes = new ArrayList<>();
 
+            // read all the mazes and add to ArrayList
             while (i < numMazes) {
                 String s = inputFile.nextLine();
 
                 if (!s.equals("")) {
                     maze = maze.concat(s);
-                    // split the string every four places by using for loop
-                    // j++
                 }
                 else {
-                    System.out.println(maze);
                     mazes.add(maze);
                     maze = "";
                     i++;
-                    //j = 0;
                 }
             }
 
@@ -52,22 +82,18 @@ public class Main {
                 int direction = numChar % 4;
                 int neighborVertex;
                 if (mazes.get(0).charAt(numChar) == '0') {
-                    if (direction == 0 || direction == 1) {
+                    if (direction == north || direction == south) {
                         neighborVertex = (direction == 0) ? vertexNumber - n: vertexNumber + n;
                     }
                     else {
-                        neighborVertex = (direction == 2) ? vertexNumber - 1: vertexNumber + 1;
+                        neighborVertex = (direction == west) ? vertexNumber - 1: vertexNumber + 1;
                     }
-                    if (!vertexes.containsKey(vertexNumber)) {
-                        System.out.print(numChar + " ");
-                        vertexes.put(vertexNumber, new Vertex());
-                    }
+                    if (!vertexes.containsKey(vertexNumber)) vertexes.put(vertexNumber, new Vertex());
                     vertexes.get(vertexNumber).edges.add(neighborVertex);
                 }
             }
 
-            System.out.println();
-            System.out.println(vertexes);
+            DFS(vertexes);
 
         }
         catch  (IOException excpt) {
--- src/MazeSimilarity/Vertex.java
+++ src/MazeSimilarity/Vertex.java
@@ -6,11 +6,15 @@ public class Vertex {
 
     ArrayList<Integer> edges;
     String color;
+    int discoveredTime;
+    int finishedTime;
     Vertex previous;
 
     public Vertex() {
         edges = new ArrayList<>();
         color = "white";
+        discoveredTime = 0;
+        finishedTime = 0;
         previous = null;
     }
 
