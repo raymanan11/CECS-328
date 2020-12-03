fbf897b18dfdc6e665cde64d1b3b843bf6218d3f
Raymond An <raymondan100@gmail.com>
2020-12-02 17:47:18 -0800

    Add Neighbors for 1, minimal and neither numbers

--- src/Hobbits/Main.java
+++ src/Hobbits/Main.java
@@ -5,16 +5,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Scanner inputFile = new Scanner(new File("HobbitExamples/input.txt"));
+            Scanner inputFile = new Scanner(new File("HobbitExamples/inputTest.txt"));
 
             ArrayList<BigInteger> numbers = new ArrayList<>();
+            ArrayList<BigInteger> minimalNumbers = new ArrayList<>();
+            ArrayList<BigInteger> maximalNumbers = new ArrayList<>();
+            ArrayList<BigInteger> neither = new ArrayList<>();
 
             while (inputFile.hasNextLine()) {
                 numbers.add(new BigInteger(inputFile.nextLine()));
@@ -22,33 +23,74 @@ public class Main {
 
             Collections.sort(numbers);
 
+            // System.out.println(numbers);
+
             int begin = 0;
             int end = numbers.size() - 1;
             for (int i = 0; i < numbers.size(); i++) {
-                System.out.println("Current Number being checked: " + numbers.get(i));
+                // System.out.println("NUmber being checked: " + numbers.get(i));
                 // check #'s before numbers.get(i)
                 boolean minimal = checkMinimal(numbers, begin, i);
-                System.out.println("Minimal: " + minimal);
+                // System.out.println("Minimal: " + minimal);
                 // check #'s after numbers.get(i)
                 boolean maximal = checkMaximal(numbers, end, i);
-                System.out.println("Maximal: " + maximal);
+                // System.out.println("Maximal: " + maximal);
+                if (minimal) minimalNumbers.add(numbers.get(i));
+                if (maximal) maximalNumbers.add(numbers.get(i));
+                if (!maximal && !minimal) neither.add(numbers.get(i));
+
             }
+
+            System.out.println("Miminal #'s: " + minimalNumbers);
+            System.out.println("Maximal #'s: " +maximalNumbers);
+            System.out.println("Neither: " +neither);
+
+            Map<BigInteger, Vertex> vertexes = new HashMap<>();
+
+            // add all minimum numbers as places to go from pad 1
+            vertexes.put(new BigInteger("1"), new Vertex(minimalNumbers));
+
+            // add neighbors for each minimal #
+            for (int i = 0; i < minimalNumbers.size(); i++) {
+                vertexes.put(minimalNumbers.get(i), new Vertex());
+                // add neighbors from max
+                for (int j = 0; j < maximalNumbers.size(); j++) {
+                    if (minimalNumbers.get(i).gcd(maximalNumbers.get(j)).intValue() > 1) vertexes.get(minimalNumbers.get(i)).neighbors.add(maximalNumbers.get(j));
+                }
+                // add neighbors from neither
+                for (int j = 0; j < neither.size(); j++) {
+                    if (minimalNumbers.get(i).gcd(neither.get(j)).intValue() > 1) vertexes.get(minimalNumbers.get(i)).neighbors.add(neither.get(j));
+                }
+            }
+
+            // add neighbors for each neither #
+            for (int i = 0; i < neither.size(); i++) {
+                vertexes.put(neither.get(i), new Vertex());
+                // add neighbors from max
+                for (int j = 0; j < maximalNumbers.size(); j++) {
+                    if (neither.get(i).gcd(maximalNumbers.get(j)).intValue() > 1) vertexes.get(minimalNumbers.get(i)).neighbors.add(maximalNumbers.get(j));
+                }
+            }
+
+
 
             PrintWriter outputFile = new PrintWriter(new FileWriter("output.txt"));
 
             outputFile.close();
 
-        } catch (IOException excpt) {
+        }
+        catch (IOException excpt) {
             System.out.println("File not found!");
         }
     }
 
     private static boolean checkMaximal(ArrayList<BigInteger> numbers, int end, int i) {
-//        System.out.println("Check Maximal");
+        // System.out.println("Check Maximal");
         for (int k = i + 1; k <= end; k++) {
 //            System.out.println(numbers.get(i));
 //            System.out.println(numbers.get(k));
 //            System.out.println();
+//            System.out.println("GCD: " + numbers.get(i).gcd(numbers.get(k)).intValue());
             if (numbers.get(i).gcd(numbers.get(k)).intValue() > 1) {
                 return false;
             }
@@ -57,11 +99,12 @@ public class Main {
     }
 
     private static boolean checkMinimal(ArrayList<BigInteger> numbers, int begin, int i) {
-//        System.out.println("Check minimal");
+        // System.out.println("Check minimal");
         for (int j = begin; j < i; j++) {
 //            System.out.println(numbers.get(j));
 //            System.out.println(numbers.get(i));
 //            System.out.println();
+//            System.out.println("GCD: " + numbers.get(j).gcd(numbers.get(i)).intValue());
             if (numbers.get(j).gcd(numbers.get(i)).intValue() > 1) {
                 return false;
             }
---
+++ src/Hobbits/Vertex.java
@@ -0,0 +1,26 @@
+package Hobbits;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Vertex {
+
+    ArrayList<BigInteger> neighbors;
+    String color;
+    int discoveredTime;
+    BigInteger previous;
+
+    public Vertex() {
+        neighbors = new ArrayList<>();
+        color = "white";
+        discoveredTime = -1;
+        previous = null;
+    }
+
+    public Vertex(ArrayList<BigInteger> neighbors) {
+        this.neighbors = neighbors;
+        color = "white";
+        discoveredTime = -1;
+        previous = null;
+    }
+}
